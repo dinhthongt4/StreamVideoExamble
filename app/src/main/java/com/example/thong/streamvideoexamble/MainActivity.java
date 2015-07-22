dfebcfdc0d263b4d747d322c6029f075d30dcbc6
ThongTD <dinhthongt4@gmail.com>
2015-07-22 09:50:14 +0700

    First push project

---
+++ app/src/main/java/com/example/thong/streamvideoexamble/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.thong.streamvideoexamble;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends ActionBarActivity {
+
+}
