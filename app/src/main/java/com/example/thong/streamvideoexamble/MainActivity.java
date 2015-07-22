659f32040dfc39c20c1b9f81906184bbb4178a94
ThongTD <dinhthongt4@gmail.com>
2015-07-22 10:50:01 +0700

    Demo Video stream

--- app/src/main/java/com/example/thong/streamvideoexamble/MainActivity.java
+++ app/src/main/java/com/example/thong/streamvideoexamble/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.thong.streamvideoexamble;
 
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.VideoView;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends ActionBarActivity {
 
+    @ViewById(R.id.myVideoView)
+    VideoView mVideoView;
+
+    @AfterViews
+    public void initValues() {
+        String videoAndress =  "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
+        Uri uri = Uri.parse(videoAndress);
+        mVideoView.setVideoURI(uri);
+        mVideoView.start();
+    }
 }
